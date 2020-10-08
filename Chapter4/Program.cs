474f079fe8a3ff592e321c36557c3995cb8c50f6
泰生 湯本 <31074@ojs.ac.jp>
2020-10-08 10:59:22 +0900

    p177まで

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -8,24 +8,49 @@ using System.Threading.Tasks;
 namespace Chapter4 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine(GetProduct());
-        }
-
-        private static string GetProduct() {
-            Sale sale = new Sale {
-                ShopName = "pet store",
-                Amount = 100000,
-                Product = "food"
+            var YearMonths = new YearMonth[] {
+                new YearMonth(1990,1),
+                new YearMonth(1991,2),
+                new YearMonth(1992,3),
+                new YearMonth(1993,4),
+                new YearMonth(1994,5),
             };
-            return sale?.Product;
-        }
-    }
 
-    class Sale {
-        //店舗名
-        public string ShopName { get; set; }
-        //売上高
-        public int Amount { get; set; }
-        public string Product { get; set; }
+            //423
+            Exercise2_2(YearMonths);
+
+            ///424
+            Console.WriteLine(FindFirst21C(YearMonths));
+        }
+
+        private static void Exercise2_2(YearMonth[] yearMonths){
+            foreach (var ym in yearMonths) {
+                Console.WriteLine(ym);
+            }
+        }
+
+        static YearMonth FindFirst21C(YearMonth[] yms) {
+            foreach (var ym in yms) {
+                if (ym.Is21Century)
+                    return ym;
+            }
+            return null;
+        }
+
+        private static void Exercise2_4(YearMonth[] ymCollection){
+            var yearmonth = FindFirst21C(ymCollection);
+            if (yearmonth == null) {
+                Console.WriteLine("21世紀のデータはありません");
+            } else {
+                Console.WriteLine(yearmonth);
+            }
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection){
+            var array = ymCollection.Select(ym => ym.AddOnMonth()).ToArray();
+            foreach (var ym in array) {
+                Console.WriteLine(ym);
+            }
+        }
     }
 }
---
+++ Chapter4/YearMonth.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4 {
+
+    class YearMonth {
+
+        //プロパティ
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+        public bool Is21Century {
+            get {
+                return 2001 <= Year && Year <= 2100;
+            }
+        }
+
+        //コンストラクタ
+        public YearMonth(int year,int month) {
+            Year = year;
+            Month = month;
+        }
+
+        public YearMonth AddOnMonth() {
+            var yearMonth = new YearMonth(this.Year, this.Month + 1);
+            if (yearMonth.Month >= 12) {
+                yearMonth.Year++;
+                yearMonth.Month = 1;
+            } else {
+                yearMonth.Month++;
+            }
+            return yearMonth;
+        }
+
+        public override string ToString() {
+            return $"{Year}年{Month}月";
+        }
+    }
+}
